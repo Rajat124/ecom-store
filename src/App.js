7e1747aaafc91e4b7cf689a0bd900dcc3af84a48
Rajat Sundriyal <rajatsundriyal1@gmail.com>
2023-08-14 13:11:37 +0530

    Lazy Loading Added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { lazy } from "react";
 import Root from "./components/Root";
-import Product from "./components/pages/Product";
-import AboutUs from "./components/pages/AboutUs";
+// import Product from "./components/pages/Product";
+// import AboutUs from "./components/pages/AboutUs";
+// import ContactUs from "./components/pages/ContactUs";
 import Home from "./components/pages/Home";
-import ContactUs from "./components/pages/ContactUs";
 import ProductDetails from "./components/pages/ProductDetails";
 import {
   Redirect,
@@ -12,6 +12,11 @@ import {
 } from "react-router-dom/cjs/react-router-dom.min";
 import Authform from "./components/Auth/Authform";
 import { AuthContext } from "./components/context/Context";
+import { Suspense } from "react";
+
+const ContactUs = lazy(() => import("./components/pages/ContactUs"));
+const AboutUs = lazy(() => import("./components/pages/AboutUs"));
+const Product = lazy(() => import("./components/pages/Product"));
 
 function App() {
   const authCtx = AuthContext();
@@ -25,14 +30,22 @@ function App() {
           <Home />
         </Route>
         <Route path="/product" exact>
-          {authCtx.isUserLoggedIn && <Product />}
+          {authCtx.isUserLoggedIn && (
+            <Suspense fallback="...Loading">
+              <Product />
+            </Suspense>
+          )}
           {!authCtx.isUserLoggedIn && <Redirect to="/auth" />}
         </Route>
         <Route path="/contactUs">
-          <ContactUs />
+          <Suspense fallback="...Loading">
+            <ContactUs />
+          </Suspense>
         </Route>
         <Route path="/AboutUs">
-          <AboutUs />
+          <Suspense fallback="...Loading">
+            <AboutUs />
+          </Suspense>
         </Route>
         <Route path="/auth">
           {!authCtx.isUserLoggedIn && <Authform />}
